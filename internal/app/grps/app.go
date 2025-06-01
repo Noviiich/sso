767a5b0c59eaeeadceb2987cbbefa2255c686382
Noviiich <nowikoff63@gmail.com>
2025-06-01 19:49:18 +0300

    реализация gRPC сервера приложения

---
+++ internal/app/grps/app.go
@@ -0,0 +1,64 @@
+package grps
+
+import (
+	"fmt"
+	"log/slog"
+	"net"
+
+	authrpc "github.com/Noviiich/sso/internal/grpc/auth"
+	"google.golang.org/grpc"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+func New(
+	log *slog.Logger,
+	port int,
+) *App {
+	gRPCServer := grpc.NewServer()
+
+	authrpc.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
+
+// MustRun запускает gRPC сервер и вызывает panic в случае ошибки.
+func (a *App) MustRun() {
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (a *App) Run() error {
+	const op = "grpsapp.Run"
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	a.log.Info("grpc server started", slog.String("addr", l.Addr().String()))
+
+	if err := a.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (a *App) Stop() {
+	const op = "grpsapp.Stop"
+
+	a.log.With(slog.String("op", op)).
+		Info("stopping gRPC server", slog.Int("port", a.port))
+
+	a.gRPCServer.GracefulStop()
+}
