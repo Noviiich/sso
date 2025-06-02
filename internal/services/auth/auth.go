a3fc125f208f4644cfb2a8a992593b8985df21d9
Alex Novikov <150938218+Noviiich@users.noreply.github.com>
2025-06-02 06:32:12 +0000

    add: Реализовал интерфейсы сервисного слоя

---
+++ internal/services/auth/auth.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/Noviiich/sso/internal/domain/models"
+)
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (int64, error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
