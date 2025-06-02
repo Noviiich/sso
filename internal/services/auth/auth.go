f092f5d2a90e751a66ebc1281a7fd07c48acd7d5
Alex Novikov <150938218+Noviiich@users.noreply.github.com>
2025-06-02 06:56:29 +0000

    add: Создание Auth структуры и функции для регистрации нового пользователя

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -2,9 +2,22 @@ package auth
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
+	"time"
 
 	"github.com/Noviiich/sso/internal/domain/models"
+	"github.com/Noviiich/sso/internal/lib/logger/sl"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
 
 type UserSaver interface {
 	SaveUser(
@@ -21,4 +34,55 @@ type UserProvider interface {
 
 type AppProvider interface {
 	App(ctx context.Context, appID int) (models.App, error)
+}
+
+func New(
+	log *slog.Logger,
+	usrSaver UserSaver,
+	usrProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:         log,
+		usrSaver:    usrSaver,
+		usrProvider: usrProvider,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL,
+	}
+}
+
+// RegisterNewUser регистрирует нового пользователя в системе и возвращает его ID.
+// Если регистрация не удалась, возвращает ошибку.
+func (a *Auth) RegisterNewUser(
+	ctx context.Context,
+	email string,
+	pass string,
+) (int64, error) {
+	// op - метка для логирования
+	const op = "Auth.RegisterNewUser"
+
+	// Создание контекста для логирования
+	log := a.log.With(
+		slog.String("op", op),       //имя операции
+		slog.String("email", email), // email пользователя
+	)
+
+	log.Info("registering new user")
+
+	// генерация хэша и соли для пароля
+	passHash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to hash password", sl.Err(err))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	// Созраняем пароль в БД
+	id, err := a.usrSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save user", sl.Err(err))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
 }
